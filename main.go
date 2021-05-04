a4d1b7995805c5f3c11b6bca88100b38f6f2b9a3
root <root@raspberrypi>
2021-05-04 23:18:57 +0300

    check relay state

--- main.go
+++ main.go
@@ -19,15 +19,27 @@ const (
 func main() {
         var light_effect float64 = 50.0
         //fmt.Println(light)
-        getsum:=getLightsum()
-        fmt.Println(getsum)
-        if getsum <= light_effect  {
-            sendGet("http://admin:admin@192.168.71.117/protect/rb0n.cgi")
-            fmt.Println("Relay ON")
-        } else {
-            sendGet("http://admin:admin@192.168.71.117/protect/rb0f.cgi") 
-            fmt.Println("Relay OFF")
+        getsum:= getLightsum()
+        getstate:= getRelaystate()
+        //если из базы вернулся ноль, то ничего не делаем
+        if getsum == 0{
+           fmt.Println("return sum = 0. Wrong sum query.Check db and inserter service!!! ")
+        }else{
+              if getsum <= light_effect && getstate==0 {
+                 sendGet("http://admin:admin@192.168.71.117/protect/rb0n.cgi")
+                 fmt.Println("Relay ON")
+              }else if getsum>=light_effect && getstate==1 {
+                 sendGet("http://admin:admin@192.168.71.117/protect/rb0f.cgi") 
+                 fmt.Println("Relay OFF")
+              }
+
         }
+
+
+
+
+        fmt.Println(getsum)
+
 }
 
 //получаем сумму значений датчика за последнии 15 мин
@@ -38,7 +50,7 @@ func getLightsum() float64 {
         checkErr(err)
         defer db.Close()
 
-	row := db.QueryRow("SELECT COALESCE(sum(light_val),0) light_date FROM light WHERE light_date BETWEEN NOW()- INTERVAL '15 minutes' and NOW()")
+	row := db.QueryRow("SELECT COALESCE(sum(light_val),0) FROM light WHERE light_date BETWEEN NOW()- INTERVAL '15 minutes' and NOW()")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,6 +64,36 @@ func getLightsum() float64 {
 
 	return lightsum
 }
+
+
+
+//получаем состояние реле
+func getRelaystate() int {
+        dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+            DB_USER, DB_PASSWORD, DB_NAME)
+        db, err := sql.Open("postgres", dbinfo)
+        checkErr(err)
+        defer db.Close()
+
+	row := db.QueryRow("SELECT COALESCE(r_state,0)  FROM relays WHERE r_id=1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer row.Close()
+	var r_state int
+	if err := row.Scan(&r_state); err != nil {
+	     // Check for a scan error.
+	     // Query rows will be closed with defer.
+		log.Fatal(err)
+       }
+
+	return r_state
+}
+
+
+
+
+
 
 //отправка запросов
 func sendGet(host string){
